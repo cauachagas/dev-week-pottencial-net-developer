67962033f90e809ee1297aa5ca6d0eef97c8eb7f
cauachagas <caua.geof@gmail.com>
2022-09-01 00:43:37 -0400

    Add a new controller class for test

---
+++ src/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController
+{
+    [HttpGet]
+    public string Hello()
+    {
+        return "Olá, Mundo!";
+    }
+}
