02040e83c7c00eab817112665777a0d92ab288fa
cauachagas <caua.geof@gmail.com>
2022-09-01 02:05:34 -0400

    Create a Person class in Models

---
+++ src/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace src.Models;
+
+public class Person
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Cpf { get; set; }
+    public bool Ativo { get; set; }
+}
