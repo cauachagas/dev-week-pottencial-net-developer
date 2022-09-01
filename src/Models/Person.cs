d0a574e63b986489d533628255137a9bd63dbdb2
cauachagas <caua.geof@gmail.com>
2022-09-01 02:09:46 -0400

    Person Cpf as nullable

--- src/Models/Person.cs
+++ src/Models/Person.cs
@@ -17,6 +17,6 @@ public class Person
 
     public string Nome { get; set; }
     public int Idade { get; set; }
-    public string Cpf { get; set; }
+    public string? Cpf { get; set; }
     public bool Ativo { get; set; }
 }
