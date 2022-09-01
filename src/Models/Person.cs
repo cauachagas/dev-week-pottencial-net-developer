2237e687356880ee392b459d48a09409653f6775
cauachagas <caua.geof@gmail.com>
2022-09-01 02:06:01 -0400

    Person constructor without params

--- src/Models/Person.cs
+++ src/Models/Person.cs
@@ -2,6 +2,12 @@ namespace src.Models;
 
 public class Person
 {
+    public Person()
+    {
+        this.Nome = "Template";
+        this.Idade = 0;
+    }
+
     public string Nome { get; set; }
     public int Idade { get; set; }
     public string Cpf { get; set; }
