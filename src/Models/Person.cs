e2abd921ae3e4fa842edd275cdd553b627df41c2
cauachagas <caua.geof@gmail.com>
2022-09-01 02:53:07 -0400

    Rename Person property Ativo to Ativado

--- src/Models/Person.cs
+++ src/Models/Person.cs
@@ -18,5 +18,5 @@ public class Person
     public string Nome { get; set; }
     public int Idade { get; set; }
     public string? Cpf { get; set; }
-    public bool Ativo { get; set; }
+    public bool Ativado { get; set; }
 }
