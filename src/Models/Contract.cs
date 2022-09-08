c5d50f6f5290e415526b2acbcbfdd763ab156c91
cauachagas <caua.geof@gmail.com>
2022-09-08 01:54:08 -0400

    Add Pago property on Contract Model

--- src/Models/Contract.cs
+++ src/Models/Contract.cs
@@ -7,6 +7,7 @@ public class Contract
     this.DataCriacao = DateTime.Now;
     this.Valor = 0;
     this.TokenID = "000000";
+    this.Pago = false;
   }
 
   public Contract(string TokenID, double Valor)
@@ -14,9 +15,11 @@ public class Contract
     this.DataCriacao = DateTime.Now;
     this.TokenID = TokenID;
     this.Valor = Valor;
+    this.Pago = false;
   }
 
   public DateTime DataCriacao { get; set; }
   public string TokenID { get; set; }
   public double Valor { get; set; }
+  public bool Pago { get; set; }
 }
