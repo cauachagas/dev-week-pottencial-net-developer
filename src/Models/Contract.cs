aad6fb91c7bed354e346f1e7f2cd32ff865a7cd2
cauachagas <caua.geof@gmail.com>
2022-09-08 01:49:16 -0400

    Add a new construct to Contract

--- src/Models/Contract.cs
+++ src/Models/Contract.cs
@@ -8,6 +8,14 @@ public class Contract
     this.Valor = 0;
     this.TokenID = "000000";
   }
+
+  public Contract(string TokenID, double Valor)
+  {
+    this.DataCriacao = DateTime.Now;
+    this.TokenID = TokenID;
+    this.Valor = Valor;
+  }
+
   public DateTime DataCriacao { get; set; }
   public string TokenID { get; set; }
   public double Valor { get; set; }
